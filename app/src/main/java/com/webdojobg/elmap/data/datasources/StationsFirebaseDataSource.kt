856ff6e34d3a40efe4686673e3c0e90a9cbc60d9
Pavel Dzhalev <pdzhalev@edu.hse.ru>
2022-05-12 23:31:35 +0300

    progress on the firestore data source

--- app/src/main/java/com/webdojobg/elmap/data/datasources/StationsFirebaseDataSource.kt
+++ app/src/main/java/com/webdojobg/elmap/data/datasources/StationsFirebaseDataSource.kt
@@ -2,6 +2,8 @@ package com.webdojobg.elmap.data.datasources
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FirebaseFirestore
 import com.webdojobg.elmap.data.models.Station
 import com.webdojobg.elmap.data.models.Station.Companion.toStation
@@ -10,11 +12,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
 @Deprecated("Class currently not used. Should be later implemented using Kotlin Flows.")
-class StationsFirebaseDataSource {
+class StationsFirebaseDataSource() {
     val database = FirebaseFirestore.getInstance()
 
-    fun getStations() : List<Station> {
-
+    fun getStations() : LiveData<List<Station>> {
+        val result = MutableLiveData<List<Station>>()
 
         var stations = ArrayList<Station>()
         database.collection("stations")
@@ -40,7 +42,8 @@ class StationsFirebaseDataSource {
                 }
             }
 
-        return stations
+        result.value = stations
+        return result
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
